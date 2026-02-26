98140268d364f52cce7ce6665053e6126db646b3
shinyatakahashiort <soratobu.shinya@gmail.com>
2026-02-26 20:53:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import japanize_matplotlib
+import matplotlib_fontja
 import json
 import os
 from datetime import datetime
